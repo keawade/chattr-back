291ffd6f08b80a4442abbe44019ca04e4afc1389
Keith Wade <keawade@gmail.com>
2016-06-13 14:57:14 +0000

    Changes made on live

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require('express')
 var mongoose = require('mongoose')
 var bodyParser = require('body-parser')
 var morgan = require('morgan')
-var io = require('socket.io')(8081)
 
 var roomController = require('./controllers/room')
 var userController = require('./controllers/user')
@@ -15,13 +14,25 @@ console.log('[mongo] succesfully connected to mongodb://' + config.mongo.address
 
 var app = express()
 
+var server = require('http').createServer(app)
+var io = require('socket.io')(server)
+
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'https://chattr-front-react-keawade.c9users.io');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS, PATCH');
+    next();
+});
+
 app.use(morgan('dev'))
 
 app.use(bodyParser.urlencoded({
   extended: true
 }))
 
-console.log('[socket] listening on port 8081')
+io.set('origins', '*:*')
+
+console.log('[socket] server listening at ' + config.express.port)
 io.on('connection', function (socket) {
   console.log('[socket] user connected')
   socket.on('message', function (message) {
@@ -58,5 +69,5 @@ router.route('/')
 
 app.use('/api', router)
 
-app.listen(config.express.port)
+server.listen(config.express.port)
 console.log('[server] running at port ' + config.express.port)
