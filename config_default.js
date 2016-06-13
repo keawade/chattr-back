7a587e9d2e6a2a98209e567f71b674c62a2760c2
Keith Wade <keithwade@nanonation.net>
2016-06-13 10:29:56 -0500

    Updated env access

--- config_default.js
+++ config_default.js
@@ -5,10 +5,10 @@ config.express.address = process.env.ADDRESS || 'localhost'
 config.express.port = process.env.PORT || 3000
 
 config.mongo = {}
-config.mongo.address = 'localhost'
-config.mongo.port = '27017'
-config.mongo.database = 'chattr'
-config.mongo.username = ''
-config.mongo.password = ''
+config.mongo.address = process.env.mongoaddress
+config.mongo.port = process.env.mongoport
+config.mongo.database = process.env.mongodbname
+config.mongo.username = process.env.mongouser
+config.mongo.password = process.env.mongopassword
 
 module.exports = config
