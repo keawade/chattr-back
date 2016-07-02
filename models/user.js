d63048b2bb19fc6617f832a163f2476ed6c5362e
Keith Wade <keithwade@nanonation.net>
2016-07-02 17:52:53 -0500

    Socket authorization and partial api implemented

--- models/user.js
+++ models/user.js
@@ -1,51 +1,31 @@
 var mongoose = require('mongoose')
-var bcrypt = require('bcrypt-nodejs')
 
 var UserSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    unique: true,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true
-  },
-  activity: Number
+    email: String,
+    email_verified: Boolean,
+    name: String,
+    given_name: String,
+    family_name: String,
+    picture: String,
+    gender: String,
+    locale: String,
+    updated_at: String,
+    user_id: String,
+    nickname: String,
+    identities: [
+        {
+            provider: String,
+            expires_in: Number,
+            user_id: String,
+            connection: String,
+            isSocial: Boolean
+        }
+    ],
+    created_at: String,
+    last_ip: String,
+    last_login: String,
+    logins_count: Number,
+    blocked_for: [String]
 })
-
-UserSchema.pre('save', function (callback) {
-  var user = this
-
-  if (!user.isModified('password')) {
-    return callback()
-  }
-
-  bcrypt.genSalt(5, function (err, salt) {
-    if (err) {
-      return callback(err)
-    }
-    bcrypt.hash(user.password, salt, null, function (err, hash) {
-      if (err) {
-        return callback(err)
-      }
-      user.password = hash
-      callback()
-    })
-  })
-})
-
-UserSchema.methods.verifyPassword = function (password, callback) {
-  bcrypt.compare(password, this.password, function (err, isMatch) {
-    if (err) {
-      return callback(err)
-    }
-    callback(null, isMatch)
-  })
-}
 
 module.exports = mongoose.model('User', UserSchema)
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+'use strict'
 var express = require('express')
 var app = express()
 var http = require('http').Server(app)
@@ -7,8 +8,17 @@ var mongoose = require('mongoose')
 var bodyParser = require('body-parser')
 var morgan = require('morgan')
 var dotenv = require('dotenv')
+var request = require('request')
+var entities = require('html-entities').AllHtmlEntities
+
+var User = require('./models/user')
 
 dotenv.load()
+
+const apiConfig = {
+  baseUrl: `https://${process.env.AUTH0_DOMAIN}/api/v2`,
+  bearerToken: process.env.API_BEARER_TOKEN
+}
 
 var env = {
   AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
@@ -26,11 +36,33 @@ app.use(bodyParser.urlencoded({
 
 // Sockets
 io
-  .on('connection', socketioJwt.authorize({
+  .on('connection', socketioJwt.authorize({ // Socket authorization
     secret: Buffer(process.env.AUTH0_CLIENT_SECRET, 'base64'),
     timeout: 15000 // 15 seconds to send the authentication message
   })).on('authenticated', function (socket) {
-    console.log('[auth] socket authenticated', JSON.stringify(socket.decoded_token))
+    let userId = entities.decode(JSON.stringify(socket.decoded_token.sub))
+    console.log('[auth] socket authenticated:', userId)
+    User.findOne({'user_id': userId}, (err, user) => {
+      if (err) {
+        console.error('[mongo] err:', err)
+      }
+      if (!user) {
+        console.log(`[mongo] did not find user for id: ${userId}`)
+        request(`${apiConfig.baseUrl}/users/${userId}`)
+          .auth(null, null, true, apiConfig.bearerToken)
+          .on('response', (response) => {
+            console.log('got response', response.statusCode)
+          })
+          .on('error', (error) => {
+            console.error('error', error)
+          })
+      } else {
+        
+      }
+    })
+    socket.on('room connect', (data) => {
+
+    })
     socket.on('chat message', function (msg) {
       io.emit('chat message', msg);
     });
